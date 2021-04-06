fc8c5e4c58cf51ac5d25d93235d5d6f21adf75eb
thaitrann <tranthai6693@gmail.com>
2021-04-06 16:33:37 +0700

    update orderwithBanking

--- app/models/order_model.py
+++ app/models/order_model.py
@@ -6,21 +6,22 @@ from marshmallow_sqlalchemy import ModelSchema
 
 class Order(mysql_db.Model):
     __tablename__ = 'orders'
-    oder_id = mysql_db.Column(mysql_db.String(20), primary_key = True)
+    order_id = mysql_db.Column(mysql_db.String(20), primary_key = True)
     status = mysql_db.Column(mysql_db.String(20))
-    create_dt = mysql_db.Column(mysql_db.String(20))
-    user_do_oder = mysql_db.Column(mysql_db.String(20))
+    create_dt = mysql_db.Column(mysql_db.String(100))
+    user_do_order = mysql_db.Column(mysql_db.String(20))
     ship_fee = mysql_db.Column(mysql_db.Integer())
     total_product = mysql_db.Column(mysql_db.Integer())
 
-    def __init__(self,order_id,status,create_dt,user_do_oder,ship_fee):
-        self.oder_id = order_id
+    def __init__(self,order_id,status,create_dt,user_do_order,ship_fee,total_product):
+        self.order_id = order_id
 
         self.status = status
         self.create_dt =create_dt
-        self.user_do_oder = user_do_oder
+        self.user_do_order = user_do_order
 
         self.ship_fee = ship_fee
+        self.total_product = total_product
 
     def create(self):
         mysql_db.session.add(self)
@@ -29,29 +30,30 @@ class Order(mysql_db.Model):
 
     def to_json(self):
         return {
-            "oder_id":self.oder_id,
+            "order_id":self.order_id,
 
             "status":self.status,
             "create_dt":self.create_dt,
-            "user_do_oder":self.user_do_oder,
+            "user_do_order":self.user_do_order,
 
             "ship_fee":self.ship_fee,
+            "total_product":self.total_product,
         }
 
     def __repr__(self):
-        return '<Order %r, %r, %r, %r, %r, %r>' % (self.oder_id, self.status, self.create_dt,self.user_do_oder,self.ship_fee,self.total_product)
+        return '<Order %r, %r, %r, %r, %r, %r>' % (self.order_id, self.status, self.create_dt,self.user_do_order,self.ship_fee,self.total_product)
 
 class OrderSchema(ModelSchema):
     class Meta(ModelSchema.Meta):
         model = Order
         sqla_session = mysql_db.session
 
-    oder_id = fields.String(dump_only=True)
+    order_id = fields.String(dump_only=True)
 
     status = fields.String(required=False)
     create_dt = fields.String(required=False)
-    user_do_oder = fields.String(required=False)
+    user_do_order = fields.String(required=False)
 
     ship_fee = fields.String(required=False)
-    total_product = fields.Integer()
+    total_product = fields.Float()
     
