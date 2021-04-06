1b5873a3f5bcec431ea7dead838b46ee196c2fa7
huy le <huy.cdl@cyberlogitec.com>
2021-04-06 16:41:53 +0700

    change oder to order

--- app/models/order_model.py
+++ app/models/order_model.py
@@ -6,7 +6,7 @@ from marshmallow_sqlalchemy import ModelSchema
 
 class Order(mysql_db.Model):
     __tablename__ = 'orders'
-    oder_id = mysql_db.Column(mysql_db.String(20), primary_key = True)
+    order_id = mysql_db.Column(mysql_db.String(20), primary_key = True)
     status = mysql_db.Column(mysql_db.String(20))
     create_dt = mysql_db.Column(mysql_db.String(20))
     user_do_oder = mysql_db.Column(mysql_db.String(20))
@@ -14,7 +14,7 @@ class Order(mysql_db.Model):
     total_product = mysql_db.Column(mysql_db.Integer())
 
     def __init__(self,order_id,status,create_dt,user_do_oder,ship_fee):
-        self.oder_id = order_id
+        self.order_id = order_id
 
         self.status = status
         self.create_dt =create_dt
@@ -29,24 +29,23 @@ class Order(mysql_db.Model):
 
     def to_json(self):
         return {
-            "oder_id":self.oder_id,
-
+            "oder_id":self.order_id,
             "status":self.status,
             "create_dt":self.create_dt,
             "user_do_oder":self.user_do_oder,
-
             "ship_fee":self.ship_fee,
+            "total_product":self.total_product
         }
 
     def __repr__(self):
-        return '<Order %r, %r, %r, %r, %r, %r>' % (self.oder_id, self.status, self.create_dt,self.user_do_oder,self.ship_fee,self.total_product)
+        return '<Order %r, %r, %r, %r, %r, %r>' % (self.order_id, self.status, self.create_dt,self.user_do_oder,self.ship_fee,self.total_product)
 
 class OrderSchema(ModelSchema):
     class Meta(ModelSchema.Meta):
         model = Order
         sqla_session = mysql_db.session
 
-    oder_id = fields.String(dump_only=True)
+    order_id = fields.String(dump_only=True)
 
     status = fields.String(required=False)
     create_dt = fields.String(required=False)
