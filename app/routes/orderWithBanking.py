db8638d68e9fe09c54ff4a5c4ef9c3044007f740
thaitrann <tranthai6693@gmail.com>
2021-04-07 00:38:46 +0700

    update orderwithbanking final

--- app/routes/orderWithBanking.py
+++ app/routes/orderWithBanking.py
@@ -13,8 +13,8 @@ def orderWithBanking():
     Order_paid_banking = {
     "status_payment":"paid",
 	"products":[
-	{"p_id":"ip14","shop_id":"shop1"},
-    {"p_id":"ip14","shop_id":"shop1"}
+	{"p_id":"ip14","shop_id":"shop1", 'amount': 1},
+    {"p_id":"ip15","shop_id":"shop1", 'amount': 2}
 	],
     "order_user_email":"duchuy1096@gmail.com",
     "ibanking_payment_email":"duchuy1096@gmail.com",
@@ -30,17 +30,17 @@ def orderWithBanking():
             date_time = time_order.strftime("%H%M%S%d%m%Y")
 
             #add order table
-            order = Order(order_id = date_time, status_shipping = 'wait', create_dt = time_order, user_do_order = Order_paid_banking['order_user_email'], ship_fee = Order_paid_banking['ship_fee'], total_product = Order_paid_banking['total_product'])
+            order = Order(order_id = date_time, status_shipping = 'wait', status_payment = Order_paid_banking['status_payment'], create_dt = time_order, user_do_order = Order_paid_banking['order_user_email'], ship_fee = Order_paid_banking['ship_fee'], total_product = Order_paid_banking['total_product'])
             mysql_db.session.add(order)
             mysql_db.session.commit()
 
             #add order_detail table
             for item in Order_paid_banking['products']:
-                order_detail = Order_Detail(p_id = item['p_id'], order_id = date_time, shop_id = item['shop_id'], status_shipping = 'wait', order_user_email = Order_paid_banking['order_user_email'], ibanking_payment_email = Order_paid_banking['ibanking_payment_email'])
+                order_detail = Order_Detail(p_id = item['p_id'], order_id = date_time, shop_id = item['shop_id'], amount = item['amount'], status_shipping = 'wait', order_user_email = Order_paid_banking['order_user_email'], ibanking_payment_email = Order_paid_banking['ibanking_payment_email'])
                 mysql_db.session.add(order_detail)
                 mysql_db.session.commit()
 
-            return make_response(jsonify({"status":200, "message":"order completed", 'time_order':time_order, 'time': date_time}))
+            return make_response(jsonify({"status":200, "message":"order completed"}))
 
         return 'not paid'
 
